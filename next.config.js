cedd6e5a57f377ab2c7cf440a55f818b19907b5d
sreedhar <sreedharks2003@gmail.com>
2023-05-19 21:57:39 +0530

    fixed rules

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/ca",
+        destination: "https://docs.google.com/forms/d/e/1FAIpQLSevuJ7wR2xlvs6dJ3JRoRvsdPNY4idsCq-lWc6IK5VFBQbClw/viewform",
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
