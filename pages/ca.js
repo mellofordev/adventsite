a9f0c887a763d58ceffd83c87ae56b761e80200c
sreedhar <sreedharks2003@gmail.com>
2023-05-19 21:48:07 +0530

    ca link redirect

---
+++ pages/ca.js
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+export default function adventca(){
+    useEffect(() => {
+        window.location.assign('https://docs.google.com/forms/d/e/1FAIpQLSevuJ7wR2xlvs6dJ3JRoRvsdPNY4idsCq-lWc6IK5VFBQbClw/viewform')
+    },[])
+    return(
+        <>
+        </>
+    );
+}
