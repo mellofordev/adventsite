91cd2c1269000c1e5361ff9bb315aafb8ac8b9e6
sreedhar <sreedharks2003@gmail.com>
2023-05-19 21:35:43 +0530

    campus ambassador custom link

---
+++ pages/adventca23.js
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+export default function adventca(){
+    useEffect(() => {
+        window.location.assign('https://forms.gle/QEK9wDmepD99Zye77')
+    })
+    return(
+        <>
+        </>
+    );
+}
