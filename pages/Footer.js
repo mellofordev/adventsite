80d72a4349024e4372d839baec9fe0f8aad4895c
sreedhar <sreedharks2003@gmail.com>
2023-05-21 19:06:29 +0530

    Footer updated

--- pages/Footer.js
+++ pages/Footer.js
@@ -4,8 +4,7 @@ export default function Footer(){
         <h1 style={{marginLeft:30}}>Advent'23</h1>
         <div style={{display:'flex',flexDirection:'column',justifyContent:'flex-start',alignItems:'flex-start',margin:30,color:'white'}}>
           <a href="/team" style={{marginBottom:10}}>Team</a>
-          <a href="https://cultaway.in/" target={'_blank'} style={{marginBottom:10}}>Cult a way</a>
-          <a href="#" target="_blank" style={{marginBottom:10}}>Brochure</a>
+          <a href="https://instagram.com/cult_a_way/" target={'_blank'} style={{marginBottom:10}}>Cult a way</a>
           <a href="https://instagram.com/advent_sctce/" target={'_blank'} style={{marginBottom:10}}>#legacyliveson</a>
         </div>
       </div>
