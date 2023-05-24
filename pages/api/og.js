6d2df008247c47c86941248ecf89c3a44c2539f6
sreedhar <sreedharks2003@gmail.com>
2023-05-24 11:49:35 +0530

    og:image api bug fixed

--- pages/api/og.js
+++ pages/api/og.js
@@ -4,12 +4,15 @@ import {useSearchParams} from 'next/navigation';
 export const config ={
     runtime: 'edge'
 };
-export default function (){
-    const search = useSearchParams();
-    const image = search.get('url');
-    return ImageResponse(
-        <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center',height:'100%',width:'100%'}}>
+export default function (req,res){
+    const {searchParams} = new URL(req.url);
+    var url='';
+    if(searchParams.has('url')){
+        url = searchParams.get('url');
+    }
+    return new ImageResponse(
+       ( <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center',height:'100%',width:'100%'}}>
             <img src={'https://adventapi.pythonanywhere.com'+url}/>
-        </div>
+        </div>)
     );
 }
